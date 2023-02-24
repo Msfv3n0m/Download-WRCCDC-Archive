faf79defe1a8aa48859a178f8bb554e5179de3fc
Msfv3n0m <71530214+Msfv3n0m@users.noreply.github.com>
2023-02-23 23:01:55 -0500

    Update driver.ps1

--- driver.ps1
+++ driver.ps1
@@ -2,18 +2,20 @@
 This script downloads ova files from wrccdc's archive, edits their version number and prepares them to be uploaded to vSphere
 The url is specified in the get-files,py file
 #>
-#Change to your username
+mkdir $HOME\Formatted-ova
+mkdir $HOME\Original-ova
 #Download files
 $pythonScript = (Get-Item .).FullName + "\get-files.py"
 echo $cd
-Start-Process "C:\Users\<username>\AppData\Local\Microsoft\WindowsApps\PythonSoftwareFoundation.Python.3.9_qbz5n2kfra8p0\python3.exe" -ArgumentList $pythonScript -Wait
+$python-path = $(Read-Host "Please enter the path to the python executable: ")
+Start-Process $python-path -ArgumentList $pythonScript -Wait
 #Format ova
-gci "C:\Users\<username>\Downloads\Original-ova" | ?{$_.Name -like "*.ova"} | %{$newPath = "C:\Users\<username>\Downloads\Formatted-ova\" + $_.Name; & "C:\Program Files\VMWare\VMware OVF Tool\ovftool.exe" $_.FullName $newPath} 
+gci "$HOME\Downloads\Original-ova" | ?{$_.Name -like "*.ova"} | %{$newPath = "$HOME\Downloads\Formatted-ova\" + $_.Name; & "C:\Program Files\VMWare\VMware OVF Tool\ovftool.exe" $_.FullName $newPath} 
 #Change format to vmx to edit version number
-gci "C:\Users\<username>\Downloads\Formatted-ova" | %{$newPath = "C:\Users\<username>\Downloads\Formatted-vmx\" + $_.BaseName + ".vmx"; & "C:\Program Files\VMWare\VMware OVF Tool\ovftool.exe" $_.FullName $newPath} 
+gci "$HOME\Downloads\Formatted-ova" | %{$newPath = "$HOME\Downloads\Formatted-vmx\" + $_.BaseName + ".vmx"; & "C:\Program Files\VMWare\VMware OVF Tool\ovftool.exe" $_.FullName $newPath} 
 #Replace version number
-$FilePaths = $(gci "C:\Users\<username>\Downloads\Formatted-vmx\" | ?{$_.Name -like "*.vmx"})
+$FilePaths = $(gci "$HOME\Downloads\Formatted-vmx\" | ?{$_.Name -like "*.vmx"})
 $FilePaths | %{((Get-Content -path $_.FullName -Raw).replace('virtualhw.version = "14"','virtualhw.version = "13"')) | Set-Content -Path $_.FullName}
 $FilePaths | %{((Get-Content -path $_.FullName -Raw).replace('virtualhw.version = "15"','virtualhw.version = "13"')) | Set-Content -Path $_.FullName}
 #Change format to ovf to upload to vSphere
-gci "C:\Users\<username>\Downloads\Formatted-vmx" | ?{$_.FullName -like "*.vmx"}| %{$newPath = "C:\Users\<username>\Downloads\Formatted-ovf\" + $_.BaseName + ".ovf"; & "C:\Program Files\VMWare\VMware OVF Tool\ovftool.exe" $_.FullName $newPath} 
+gci "$HOME\Downloads\Formatted-vmx" | ?{$_.FullName -like "*.vmx"}| %{$newPath = "$HOME\Downloads\Formatted-ovf\" + $_.BaseName + ".ovf"; & "C:\Program Files\VMWare\VMware OVF Tool\ovftool.exe" $_.FullName $newPath} 
