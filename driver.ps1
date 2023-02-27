a28066d9124dd309934a9d64bab7648787aaa897
Msfv3n0m <71530214+Msfv3n0m@users.noreply.github.com>
2023-02-27 14:53:54 -0500

    Update driver.ps1

--- driver.ps1
+++ driver.ps1
@@ -2,8 +2,8 @@
 This script downloads ova files from wrccdc's archive, edits their version number and prepares them to be uploaded to vSphere
 The url is specified in the get-files,py file
 #>
-mkdir $HOME\Formatted-ova
-mkdir $HOME\Original-ova
+mkdir $HOME\Downloads\Formatted-ova
+mkdir $HOME\Downloads\Original-ova
 #Download files
 $pythonScript = (Get-Item .).FullName + "\get-files.py"
 echo $cd
