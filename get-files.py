ddac4d9c59a64414e4bdab72d3ce56283820c48c
Msfv3n0m <71530214+Msfv3n0m@users.noreply.github.com>
2023-02-27 15:29:03 -0500

    Update get-files.py

--- get-files.py
+++ get-files.py
@@ -7,6 +7,8 @@ import urllib.request
 from urllib.request import Request, urlopen
 import lxml
 username = os.getlogin()
+filestartswith1 = "coal"
+filestartswith2 = "dynomite"
 host_site = "https://archive.wrccdc.org/images/2020/wrccdc-2020-invitationals/rock.bottom/"
 user_agent = 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.9.0.7) Gecko/2009021910 Firefox/3.0.7'
 headers={'User-Agent':user_agent,} 
